cc70b286558772fbb457fc80f666f866a1055c0f
James Browning <thejamesernator@gmail.com>
2020-07-13 22:32:36 +1000

    .

--- install-deps.sh
+++ install-deps.sh
@@ -6,10 +6,6 @@ pushd ./binaryen/
 cmake . && make
 popd
 
-# Wasi SDK
-wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11.0-linux.tar.gz
-tar xvf wasi-sdk-11.0-linux.tar.gz
-
 # Wbat
 git clone --recursive https://github.com/WebAssembly/wabt
 pushd wabt/
