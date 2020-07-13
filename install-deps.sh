003caad7919e52277cb373f354e3ed7be710b83e
James Browning <thejamesernator@gmail.com>
2020-07-13 17:42:33 +1000

    .

--- install-deps.sh
+++ install-deps.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 git clone https://github.com/WebAssembly/binaryen ./binaryen/
+version=11
+wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11.0-linux.tar.gz
+tar xvf wasi-sdk-11.0-linux.tar.gz
