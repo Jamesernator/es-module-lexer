3d605dc8ee8c3424f1197ca09b3b3babe024efce
James Browning <thejamesernator@gmail.com>
2020-08-14 12:01:09 +1200

    .

--- CyclicModuleRecord.ts
+++ CyclicModuleRecord.ts
@@ -1,20 +1,69 @@
 import ModuleRecord from "./ModuleRecord.js";
 
-type ErrorResult = { evaluationError: any };
+type EvaluationError = { evaluationError: any };
 
-type UnlinkedState = { name: "unlinked" };
-type LinkingState = { name: "linking", dfsIndex: number };
-type LinkedState = { name: "linked" };
-type EvaluatingState = { name: "evaluating" };
-type EvaluatedState = { name: "evaluated", result: null | ErrorResult };
+type CyclicModuleState =
+    | "unlinked"
+    | "linking"
+    | "linked"
+    | "evaluating"
+    | "evaluated";
 
-type ModuleState =
-    | UnlinkedState
-    | LinkingState
-    | LinkedState
-    | EvaluatingState
-    | EvaluatedState;
+type ResolveModule = (specifier: string, module: ModuleRecord) => ModuleRecord | Promise<ModuleRecord>;
 
-export default class CyclicModuleRecord extends ModuleRecord {
-    #state: ModuleState;
+function assertExists<T>(value: T): NonNullable<T> {
+    if (value == null) {
+        throw new TypeError("Expected value not to be nullish");
+    }
+    return value as NonNullable<T>;
+}
+
+type CyclicModuleRecordOptions = {
+    resolveModule: ResolveModule,
+    getRequiredModules: () => Promise<Array<string>>,
+    initializeEnvironment: () => void,
+    executeModule: () => void,
+};
+
+export default abstract class CyclicModuleRecord extends ModuleRecord {
+    #state: CyclicModuleState = "unlinked";
+    #evaluationError: null | EvaluationError = null;
+    #dfsIndex?: number;
+    #dfsAncestorIndex?: number;
+    #resolveModule: ResolveModule;
+    #getRequiredModules: () => Promise<Array<string>>;
+    #initializeEnvironment: () => void;
+    #executeModule: () => void;
+
+    constructor(options: CyclicModuleRecordOptions) {
+        super();
+        this.#resolveModule = options.resolveModule;
+        this.#getRequiredModules = options.getRequiredModules;
+        this.#initializeEnvironment = options.initializeEnvironment;
+        this.#executeModule = options.executeModule;
+    }
+
+    async link(): Promise<void> {
+        if (this.#state !== "unlinked") {
+            return;
+        }
+        this.#state = "linking";
+        for (const requiredSpecifier of await this.#getRequiredModules()) {
+            const requiredModule = await this.#resolveModule(
+                requiredSpecifier,
+                this,
+            );
+            await requiredModule.link();
+        }
+        this.#state = "linked";
+    }
+
+    async evaluate(): Promise<void> {
+        if (this.#state === "evaluated" || this.#state === "evaluating") {
+            return;
+        }
+        if (this.#state !== "linked") {
+            throw new Error("Can't evaluate module wh;ich is not linked or currently executing");
+        }
+    }
 }
