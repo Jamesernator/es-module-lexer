e0db5a911b87c7b47b46695504f40990a5e38612
James Browning <thejamesernator@gmail.com>
2020-10-05 17:46:09 +1300

    .

--- Module.ts
+++ Module.ts
@@ -27,6 +27,82 @@ export type ModuleOptions = {
     resolveExport: ResolveExport,
 };
 
+function isAccessorDescriptor(descriptor: PropertyDescriptor) {
+    return Boolean(descriptor.get ?? descriptor.set);
+}
+
+function createModuleNamespace(resolvedExports: Map<string, () => any>) {
+    const sortedExportNames = Array.from(resolvedExports.keys()).sort();
+    const target = Object.create(null);
+    Object.defineProperty(target, Symbol.toStringTag, {
+        value: "Module",
+    });
+    for (const exportName of sortedExportNames) {
+        const getBinding = resolvedExports.get(exportName)!;
+        Object.defineProperty(target, exportName, {
+            value: getBinding(),
+            enumerable: true,
+            writable: true,
+            configurable: false,
+        });
+    }
+    Object.preventExtensions(target);
+    return new Proxy(target, {
+        setPrototypeOf(target, proto) {
+            return proto === null;
+        },
+        getOwnPropertyDescriptor(target, prop: string | symbol) {
+            if (typeof prop === "symbol") {
+                return Reflect.getOwnPropertyDescriptor(target, prop);
+            }
+            if (!Reflect.has(target, prop)) {
+                return undefined;
+            }
+            const getBinding = resolvedExports.get(prop)!;
+            Reflect.set(target, prop, getBinding());
+            return Reflect.getOwnPropertyDescriptor(target, prop);
+        },
+        defineProperty(target, prop, descriptor) {
+            if (typeof prop === "symbol") {
+                return Reflect.defineProperty(target, prop, descriptor);
+            }
+            const current = Reflect.getOwnPropertyDescriptor(target, prop);
+            if (current === undefined) {
+                return false;
+            }
+            if (isAccessorDescriptor(current)) {
+                return false;
+            }
+            if (descriptor.writable !== undefined
+            && !descriptor.writable) {
+                return false;
+            }
+            if (descriptor.enumerable !== undefined
+            && !descriptor.enumerable) {
+                return false;
+            }
+            if (descriptor.configurable !== undefined
+            && descriptor.configurable) {
+                return false;
+            }
+            if ("value" in descriptor) {
+                return Object.is(descriptor.value, current.value);
+            }
+            return true;
+        },
+        get(target, prop: string | symbol, receiver) {
+            if (typeof prop === "symbol") {
+                return Reflect.get(target, prop, receiver);
+            }
+            const getBinding = resolvedExports.get(prop);
+            return getBinding?.();
+        },
+        set() {
+            return true;
+        },
+    });
+}
+
 export default abstract class Module {
     readonly #link: () => void | Promise<void>;
     readonly #evaluate: () => void;
@@ -63,20 +139,16 @@ export default abstract class Module {
         if (this.#namespace) {
             return this.#namespace;
         }
-        const namespace = Object.create(null);
-        namespace[Symbol.toStringTag] = "Module";
-        for (const exportName of this.#getExportedNames(new Set())) {
-            const resolvedBinding = this.#resolveExport(exportName, []);
-            if (resolvedBinding !== null && resolvedBinding !== AMBIGUOUS) {
-                Object.defineProperty(namespace, exportName, {
-                    enumerable: true,
-                    get: resolvedBinding.getBinding,
-                });
+        const resolvedExports = new Map<string, () => any>();
+        const exportedNames = this.#getExportedNames(new Set());
+        for (const exportName of exportedNames) {
+            const resolvedExport = this.#resolveExport(exportName, []);
+            if (resolvedExport !== AMBIGUOUS && resolvedExport !== null) {
+                resolvedExports.set(exportName, resolvedExport.getBinding);
             }
         }
-        this.#namespace = namespace;
-        Object.freeze(namespace);
-        return namespace;
+        this.#namespace = createModuleNamespace(resolvedExports);
+        return this.#namespace;
     };
 
     get namespace(): any {
